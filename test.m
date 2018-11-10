b161de90f12ed48f2d7211bab7a253c013adbf1b
LiHongXiang <490793521@qq.com>
2018-11-10 18:13:39 +0800

    删除test内容

--- test.m
+++
@@ -1,4 +0,0 @@
-a = pwd ;
-b = xlsread ('C:\Users\LHX\the-seasonal-flood-periods\data\source\毛俊日流量表1973-2003.xls','1973','B3:M33');
-d = [a,'\data\source\毛俊日流量表1973-2003.xls'];
-c = xlsread ([a,'\data\source\毛俊日流量表1973-2003.xls'],'1973','B3:M33');
