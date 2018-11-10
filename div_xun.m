3004c20101fdf3bd55e89be0524c2e52c9f1d157
LiHongXiang <490793521@qq.com>
2018-11-10 18:04:33 +0800

    添加NaN变成0

--- div_xun.m
+++ div_xun.m
@@ -1,10 +1,10 @@
-function [ a ] = div_xun( Q )
+function [ data_xun ] = div_xun( Q )
 %UNTITLED2 将数据按旬划分，同时合并年份
 %    Q(yy,dd,mm);     年/日/月
 %    data_xun(36,yy,dd);  将数据按旬重新划分 
 
 [yy,~,~] = size(Q) ;
-
+data_xun = zero(36,yy,11);
 for i = 1:36
     for j = 1:yy
         for k = 1:10
@@ -15,7 +15,13 @@ for i = 1:36
         end
     end
 end
-            
+
+
+
+ z=find(data_xun==0);
+
+data_xun(z)=[];
+
 a = sum(sum(sum(data_xun)))   -   sum(sum(sum(Q))) ;
 
 
