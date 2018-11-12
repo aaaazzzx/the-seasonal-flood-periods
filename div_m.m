2b011648259167f7dcdce2e2b369c679db950ad2
LiHongXiang <490793521@qq.com>
2018-11-12 18:06:11 +0800

    turn ans to 2D

--- div_m.m
+++ div_m.m
@@ -1,14 +1,14 @@
 function [ data_m ] = div_m( Q )
 % 将数据按月重新排序，同时合并年份
 %    Q(yy,dd,mm);     年/日/月
-%    data_m(12,yy,31);  将数据按旬重新划分 
+%    data_m(12,yy*31);  将数据按月重新划分 
 
 [yy,~,~] = size(Q) ;
-data_m = zeros(12,yy,31);
+data_m = zeros(12,yy*31);
 for i = 1:12
     for j = 1:yy
         for k = 1:31
-            data_m(i,j,k) = Q(j,k,i) ;
+            data_m(i,(j-1)*31+k) = Q(j,k,i) ;
         end
     end
 end
--- div_xun.m
+++ div_xun.m
@@ -1,17 +1,17 @@
 function [ data_xun ] = div_xun( Q )
 %UNTITLED2 将数据按旬划分，同时合并年份
 %    Q(yy,dd,mm);     年/日/月
-%    data_xun(36,yy,dd);  将数据按旬重新划分 
+%    data_xun(36,dd);  将数据按旬重新划分,dd每11个为一年，共有11*yy
 
 [yy,~,~] = size(Q) ;
-data_xun = zeros(36,yy,11);
+data_xun = zeros(36,yy*11);
 for i = 1:36
     for j = 1:yy
         for k = 1:10
-            data_xun(i,j,k) = Q(j,k+mod(i,3)*10,ceil(i/3)) ;
+            data_xun(i,(j-1)*11+k) = Q(j,k+mod(i,3)*10,ceil(i/3)) ;
         end
         if mod(i,3)==0
-            data_xun(i,j,11) = Q(j,31,ceil(i/3)) ;
+            data_xun(i,j*11) = Q(j,31,ceil(i/3)) ;
         end
     end
 end
