5c9d06f07294b6413d6a061e0747b3f525aefbae
LiHongXiang <490793521@qq.com>
2018-11-20 09:28:54 +0800

    add loadDATA

--- test.py
+++ test.py
@@ -8,14 +8,30 @@ def eachFile(filename):
         if filename in files:
             filePath = os.path.join(str(root),str(dirs))
             filePath = filePath[:-2]+filename
-            return filePath
+    return filePath
+
+
+def loadDatadet(infile):
+    f=open(infile,'r')
+    sourceInLine=f.readlines()
+    dataset=[]
+    for line in sourceInLine:
+        temp1=line.strip('\n')
+        temp2=temp1.split()
+        dataset.append(temp2)
+    for i in range(0,len(dataset)):
+        for j in range(961):    #961为每行所含数字
+            dataset[i].append(float(dataset[i][j]))
+        del(dataset[i][0:961])
+    return dataset
+
+
 
 if __name__ == '__main__':
     fileName = 'data_m.txt'
     filePath = eachFile(fileName)
     data_m = open(filePath)
     line = data_m.readline()
-    print (type(line))
-    print (len(line))
-    print (len(data_m.read_csv()))
+    infile=loadDatadet(filePath)
+ 
 
