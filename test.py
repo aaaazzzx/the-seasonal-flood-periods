a1c186246a0128b94c15f4582c560bdeb2803e3d
LiHongXiang <490793521@qq.com>
2018-11-20 11:30:11 +0800

    add test.data_now

--- test.py
+++ test.py
@@ -1,5 +1,6 @@
 import os
 import pandas
+import numpy
 
 #查找当前文件夹下的指定文件，同时返回文件目录
 def eachFile(filename): 
@@ -11,7 +12,7 @@ def eachFile(filename):
     return filePath
 
 
-def loadDatadet(infile):
+def loadDatadet(infile,k):
     f=open(infile,'r')
     sourceInLine=f.readlines()
     dataset=[]
@@ -20,10 +21,12 @@ def loadDatadet(infile):
         temp2=temp1.split()
         dataset.append(temp2)
     for i in range(0,len(dataset)):
-        for j in range(961):    #961为每行所含数字
+        for j in range(k):
             dataset[i].append(float(dataset[i][j]))
-        del(dataset[i][0:961])
+        del(dataset[i][0:k])
     return dataset
+
+
 
 
 
@@ -32,6 +35,14 @@ if __name__ == '__main__':
     filePath = eachFile(fileName)
     data_m = open(filePath)
     line = data_m.readline()
-    infile=loadDatadet(filePath)
+    infile=loadDatadet(filePath,961)  #961为每行所含数字
+    data_m = numpy.array(infile)
+    x,y= data_m.shape
+    for i in range(x):
+        for j in range(int(y/31)):
+            data_now = data_m[i,j*31:(j+1)*31]
+            data_now = data_now[data_now>0.000001]
+            
+
  
 
