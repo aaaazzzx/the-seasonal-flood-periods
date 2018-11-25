f87582340be74d4950ce7b9766281ffc9898d25b
LHX <490793521@qq.com>
2018-11-25 20:09:45 +0800

    finish treat k=2

--- treat.py
+++ treat.py
@@ -28,21 +28,24 @@ if __name__ == '__main__':
     # print (data_m_t)
     D = numpy.zeros((y,x-1,x-1))
     D2 = numpy.zeros((y,x-1,x-1))
+    D1 = numpy.zeros((y,x))
     for k in range(y):
         for i in range(x-1):
             for j in range(i+1,x):
                 print (i,j)
                 print (data_m_t[i:j+1,k])
                 D[k,i,j-1] = (j-i+1)*numpy.std(data_m_t[i:j+1,k])
-                D2[k,i,j-1] = (12-(j-i+1)) * numpy.std ( numpy.append(data_m_t[:i,k],data_m_t[j:,k]) )
-    print (D2[1,:,:])
+                #D2[k,i,j-1] = (12-(j-i+1)) * numpy.std ( numpy.append(data_m_t[:i,k],data_m_t[j:,k]) )
+                for l in range (x):
+                     D1[k, l] = 11 * numpy.std(numpy.delete(data_m_t[:, k],l))
+    # print (D2[1,:,:])
     
     file = os.getcwd()
-    f = open('%s\data\pretreatment\D2.txt'%(file),'w')
-    numpy.savetxt('%s\data\pretreatment\D2.txt'%(file),D2[1,:,:])
-    writer = pandas.ExcelWriter('output.xlsx')
+    f = open('%s\data\pretreatment\D.txt'%(file),'w')
+    numpy.savetxt('%s\data\pretreatment\D.txt'%(file),D2[1,:,:])
+    writer = pandas.ExcelWriter('output1.xlsx')
     for i in range(y):
-        df = pandas.DataFrame(D2[i,:,:])
+        df = pandas.DataFrame(D1[i,:])
         df.to_excel(writer,'Sheet%s'%(i))
         writer.save()
     f.close()
