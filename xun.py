118e787ce1373188b14e1b36f34a26bac1248de4
aaaazzzx <490793521@qq.com>
2018-11-30 09:13:39 +0800

    finish xun

--- xun.py
+++ xun.py
@@ -1,5 +1,6 @@
 import xlrd
 import numpy
+import pandas
 
 readbook = xlrd.open_workbook(r'H:\the-seasonal-flood-periods\data\source\毛俊日流量表1973-2003.xls')
 Q = numpy.zeros((31, 12, 31))  # 年，月，日
@@ -13,13 +14,15 @@ for i in range(1973,2004):    #31 年
             else :
                 Q[i - 1973, j, k] = 0
 output = numpy.zeros((12*3, 11*31))
+writer = pandas.ExcelWriter(r'旬逐日流量.xlsx')
 for i in range(12*3):    #旬j
     for j in range(31):    #年
         for k in range(10):     #日
-            output[i,j*11+k] = Q[j,int(i*3),k+i*10]
-            print(j*11+k)
-        if i % 3 == 0:
-            print (j*11+10)
-            #output[i, j*11+11] = Q[j,int(i*3),11+i*10]
-
-    #print  (output[i, :])
+            output[i,j*11+k] = Q[j,int(i/3),k+(i%3)*10]
+            #print(j,int(i/3),k+(i%3)*10)
+        if (1+i) % 3 == 0:
+            #print (j,int(i/3),10+(i%3)*10)
+            output[i, j*11+10] = Q[j,int(i/3),10+(i%3)*10]
+df = pandas.DataFrame(output)
+df.to_excel(writer, 'sheet1' )
+writer.save()
